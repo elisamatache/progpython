528ad8decce964e8dddb999e620e467768e39ea9
Elisa <150685033+elisamatache@users.noreply.github.com>
2023-11-14 12:52:31 +0100

    Add files via upload

---
+++ Tarea 1/media.py
@@ -0,0 +1,8 @@
+num1=int(input("Introduce un número:"))
+num2=int(input("Introduce otro número:"))
+num3=int(input("Introduce otro número:"))
+media=(nota1+nota2+nota3)/3
+if media>=5:
+    print("Has aprobado con una media de ",media)
+else:
+    print("Has suspendido con una media de ",media)
---
+++ Tarea 1/sumaresta.py
@@ -0,0 +1,10 @@
+num1=int(input("Introduce un número:"))
+num2=int(input("Introduce otro número:"))
+if num1>num2:
+    suma=num1+num2
+    resta=num1-num2
+    print("La suma es",suma,"y la diferencia",producto)
+else:
+    producto=num1*num2
+    división =num1//num2
+    print("El producto es ",producto,"y la división entera es",división)
