fe01ea6d6f1b9f50abc432bc0c150c8888713da9
Elisa <150685033+elisamatache@users.noreply.github.com>
2023-11-14 12:08:38 +0100

    Delete nums.py

--- nums.py
+++
@@ -1,7 +0,0 @@
-1 num1=int(input("Introduce numero 1:"))
-2 num1=int(input("Introduce numero 1:"))
-3 num1=int(input("Introduce numero 1:"))
-4 num1=int(input("Introduce numero 1:"))
-5 suma=num1+num2
-6 producto=num3*num4
-7 print("La suma es",suma,"y el producto es", producto)
