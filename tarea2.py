18bc7ad32a2b40084ab57d7d03832011ac9c6fe9
Elisa <150685033+elisamatache@users.noreply.github.com>
2023-11-14 12:09:02 +0100

    Delete tarea2.py

--- tarea2.py
+++
@@ -1,8 +0,0 @@
-n1=int(inout("Introduce un numero:"))
-n2=int(inout("Introduce un numero:"))
-n3=int(inout("Introduce un numero:"))
-n4=int(inout("Introduce un numero:"))
-suma=n1+n2+n3+n4
-media=suma/4
-print("La suma es",suma)
-print("La media es",media)
