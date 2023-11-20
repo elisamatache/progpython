c5d5d83402a7c9d6c69da4301006cb8b86f45cb2
Elisa <150685033+elisamatache@users.noreply.github.com>
2023-11-20 14:58:49 +0100

    Add files via upload

---
+++ tarea3/coordenadas.py
@@ -0,0 +1,10 @@
+coordX=int(input("Introduce coordenada X:"))
+coordY=int(input("Introduce coordenada Y:"))
+if cordX>0 and cordY>0:
+    print("Primer cuadrante")
+if cordX<0 and cordY>0:
+    print ("Segundo cuadrante")
+if cordX<0 and cordY<0:
+    print("Tercer cuadrante")
+if cordX>0 and cordY<0:
+    print("Cuarto cuadrante")
