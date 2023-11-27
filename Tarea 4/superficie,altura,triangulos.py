5aafa623baee79769bce394f09451cf01652e89a
Elisa <150685033+elisamatache@users.noreply.github.com>
2023-11-27 14:27:43 +0100

    Add files via upload

---
+++ Tarea 4/superficie,altura,triangulos.py
@@ -0,0 +1,10 @@
+n=int(input("Cuantos triángulos?"))
+for x in range(n):
+    base=float(input("Base del triángulo:"))
+    altura=float(input("Altura del triángulo:"))
+    superficie=base*altura/2
+    print("El triángulo cuya base es ",base,"y la altura",altura,"tiene una superficie:",superficie)
+    if superficie>12:
+        cantidad=cantidad+1
+
+print("Cantidad de triangulos cuya superficie es superior a 12:",cantidad
