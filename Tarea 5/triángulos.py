7659d14b5353924349379cc4b35dc24a64b105ab
Elisa <150685033+elisamatache@users.noreply.github.com>
2023-11-28 12:53:08 +0100

    Add files via upload

---
+++ Tarea 5/triángulos.py
@@ -0,0 +1,18 @@
+n=int(input("Triángulos?="))
+for x in range(n):
+    lado1=float(input("Lado 1:"))
+    lado2=float(input("Lado 2:"))
+    lado3=float(input("Lado 3:"))
+    if lado1==lado2 and lado1==lado3:
+        print("Triángulo Equilátero")
+        equilatero=equilatero+1
+    else:
+        if lado1==lado2 or lado1==lado3 or lado2==lado3:
+            print("Triángulo Isósceles")
+            isosceles=isosceles+1
+        else:
+            print("Triángulo Escaleno")
+            escaleno=escaleno+1
+print("Cantidad de equilátero:",equilatero)
+print("Cantidad de Isósceles:", isosceles)
+print("Cantidad de Escaleno:",escaleno)
