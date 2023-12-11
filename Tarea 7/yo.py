d8e44ef9bcbc1c3ea475cec6afddd59a365f3b72
Elisa <150685033+elisamatache@users.noreply.github.com>
2023-12-11 14:55:13 +0100

    Add files via upload

---
+++ Tarea 7/yo.py
@@ -0,0 +1,10 @@
+#Sumar 10 números
+#Elisa Matache
+#11 de diciembre de 2023
+#Variable acumuladora,suma
+
+suma=0
+for x in range(10):
+    num==int(input("Numero:"))
+    suma=suma+num
+print("La suma total es",suma)
