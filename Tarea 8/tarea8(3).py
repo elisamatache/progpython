d682ffc7ecf55f829461ac4759bedd103e2119cb
Elisa <150685033+elisamatache@users.noreply.github.com>
2024-01-08 14:39:25 +0100

    Add files via upload

---
+++ Tarea 8/tarea8(3).py
@@ -0,0 +1,6 @@
+clave=input("introduce una clave entre 10 y 20 caracteres:")
+while x<len(clave):
+    if longitud>=10 and longitud<=20:
+        print("La clave es correcta")
+else:
+    print("La clave es incorrecta")
---
+++ Tarea 8/tarea8.py
@@ -0,0 +1,10 @@
+oracion=input("introduce una frase:")
+palabras=1
+longitud=len(oracion)
+x=0
+while x<longitud:
+    if (oracion[x]==""):
+        palabras=palabras+1
+    x=x+1
+print("Palabras:", palabras)
+            
